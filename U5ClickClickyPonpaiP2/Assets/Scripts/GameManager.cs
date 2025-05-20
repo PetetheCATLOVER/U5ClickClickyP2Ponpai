c9d46b74598b183c852b3b7ccd4d537cd42abbd8
SeanJ07 <91712813+SeanJ07@users.noreply.github.com>
2025-05-20 09:52:21 -0700

    U5ClickClickyP2Ponpai
    half done

--- U5ClickClickyPonpaiP2/Assets/Scripts/GameManager.cs
+++ U5ClickClickyPonpaiP2/Assets/Scripts/GameManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Net.Sockets;
+using System.Xml.Schema;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -28,5 +35,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
+     public void UpdateScore(int scoreToAdd) 
+    { 
+    score += scoreToAdd;
+        scoreText.text = "Score:" + score;
+    
+    }
 
 }
